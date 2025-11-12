d7437d5eccfc67dd43b996a9b3dbc34d6902de6e
Maxim Babichev <maksim.babichev95@gmail.com>
2025-11-12 20:23:07 +0300

    Update project description in InputComponent to clarify that it is provided "as is" and that updates will be made only when necessary.

--- src/input.js
+++ src/input.js
@@ -432,7 +432,7 @@ export default class InputComponent extends React.Component {
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
                             <p className={`${isToggled ? 'text-blue-200' : 'text-blue-800'} text-sm leading-relaxed`}>
-                                The project is provided "as is". Project revisions will only be made when absolutely necessary.
+                                This project is provided "as is". Updates will only be made when absolutely necessary.
                             </p>
                         </div>
                     </div>
